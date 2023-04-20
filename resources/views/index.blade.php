d114fd8d76cd3f336e6d3916d7dde157881633cb
engyyyyy <engymakram008@gmail.com>
2023-04-20 22:00:46 +0200

    added files

---
+++ resources/views/index.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student name</title>
+</head>
+<style>
+</style>
+<body style="background-color:aliceblue;">
+<table border="1"
+ style="border: 1px solid #ccc;
+  font-size:larger;
+  width:100%">
+  <caption style="font-size: 150%; ">Students Name</caption>
+    <thead>
+        <th> Name</th>
+        <th> Sec</th>
+        <th> Depart</th>
+        <th> Email</th>
+    </thead>
+    <tbody>
+        <tr>
+            <td>engy makram</td>
+            <td> 8</td>
+            <td>CS</td>
+            <td>gogo22@gmail</td>
+        </tr>
+        <tr>
+            <td>mariam m</td>
+            <td> 2</td>
+            <td>CS</td>
+            <td>angel magdi5@gmail</td>
+        </tr>
+        <tr>
+            <td>asmaa</td>
+            <td> 9</td>
+            <td>IS</td>
+            <td>ass09@gmail</td>
+        </tr>
+        <tr>
+            <td>ennnngi</td>
+            <td> 9</td>
+            <td>CS</td>
+            <td>mgogogga@gmail</td>
+        </tr>
+        <tr>
+            <td>eeenngi</td>
+            <td> 8</td>
+            <td>CS</td>
+            <td>jjjj6@gmail</td>
+        </tr>
+        <tr>
+            <td>mariam ahmed</td>
+            <td> 6</td>
+            <td>IT</td>
+            <td>marriamm20@gmail</td>
+        </tr>
+        <tr>
+            <td>aya</td>
+            <td> 7</td>
+            <td>CS</td>
+            <td>ayaaa9@gmail</td>
+        </tr>
+        <tr>
+            <td>amirad</td>
+            <td> 10</td>
+            <td>CS</td>
+            <td>amiraa11@gmail</td>
+        </tr>
+        <tr>
+            <td>Engiiiiiiiiiiiii</td>
+            <td> 3</td>
+            <td>IS</td>
+            <td>engyAna@gmail</td>
+        </tr>
+        <tr>
+            <td>ahelad</td>
+            <td> 8</td>
+            <td>IT</td>
+            <td>ahela5@gmail</td>
+        </tr>
+        <tr>
+            <td>sarrar</td>
+            <td> 6</td>
+            <td>IT</td>
+            <td>sara2@gmail</td>
+        </tr>
+        <tr>
+            <td>nadaaaa</td>
+            <td> 6</td>
+            <td>IS</td>
+            <td>naddda33@gmail</td>
+        </tr>
+        <tr>
+            <td>aasmaa mahmoud</td>
+            <td> 9</td>
+            <td>CS</td>
+            <td>asmaad44@gmail</td>
+        </tr>
+        <tr>
+            <td>ggggg mostafa</td>
+            <td> 2</td>
+            <td>IT</td>
+            <td>am99@gmail</td>
+        </tr>
+        <tr>
+            <td>mina mm</td>
+            <td> 9</td>
+            <td>CS</td>
+            <td>mema2000@gmail</td>
+        </tr>
+        <tr>
+            <td>eeeeeee  m </td>
+            <td> 10</td>
+            <td>CS</td>
+            <td>eeeeeeeeeeee0@gmail</td>
+        </tr>
+        <tr>
+            <td>mariam makran</td>
+            <td> 9</td>
+            <td>CS</td>
+            <td>meroo30@gmail</td>
+        </tr>
+    </tbody>
+
+</table>
+
+</body>
+</html>
